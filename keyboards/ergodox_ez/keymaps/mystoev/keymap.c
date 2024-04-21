ecfeaf3f6b5412cbb55b9298b12c8729dd6c3a44
Martin Stoev <martin.y.stoev@gmail.com>
2024-04-21 19:35:59 +0300

    add experiments layer

--- keyboards/ergodox_ez/keymaps/mystoev/keymap.c
+++ keyboards/ergodox_ez/keymaps/mystoev/keymap.c
@@ -3,7 +3,8 @@
 
 enum layers {
     BASE,  // default layer
-    MOUSE,  // symbols
+    MOUSE,  // mouse
+    EXPR,  // experiments
 };
 
 enum custom_keycodes {
@@ -23,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   =  |           |  -   |------+-----+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |TO(1) |BL_TOGG| Alt | Ctrl/Left| Gui/Right|                               |Gui/Up|Ctrl/Down|Alt/[|  ] | TO(1)  |
+ *   |TO(1) |BL_TOGG| Alt | Ctrl/Left| Gui/Right|                               |Gui/Up|Ctrl/Down|Alt/[|  ] | TO(2)  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Prev | Next |       | Left | Right |
@@ -39,12 +40,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,    KC_W,        KC_E,            KC_R,    KC_T,    KC_WH_U,              KC_WH_D,     KC_Y,   KC_U,          KC_I,            KC_O,            KC_P,    KC_BSLS,
   KC_CAPS,    KC_A,    KC_S,        KC_D,            KC_F,    KC_G,                                       KC_H,   KC_J,          KC_K,            KC_L,            KC_SCLN, KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,        KC_C,            KC_V,    KC_B,    KC_EQL,               KC_MINS,     KC_N,   KC_M,          KC_COMM,         KC_DOT,          KC_SLSH, KC_RSFT,
-  TO(1),      BL_TOGG, LALT(KC_NO), LCTL_T(KC_LEFT), LGUI_T(KC_RGHT),                                             RGUI_T(KC_UP), RCTL_T(KC_DOWN), RALT_T(KC_LBRC), KC_RBRC, TO(1),
+  TO(1),      BL_TOGG, LALT(KC_NO), LCTL_T(KC_LEFT), LGUI_T(KC_RGHT),                                             RGUI_T(KC_UP), RCTL_T(KC_DOWN), RALT_T(KC_LBRC), KC_RBRC, TO(2),
                                                                  KC_MPRV, KC_MNXT,         KC_LEFT, KC_RGHT,
                                                                           KC_VOLU,         KC_UP,
                                                          KC_BSPC, KC_ESC, KC_VOLD,         KC_DOWN, KC_ENT, KC_SPC
 ),
-/* Keymap 1: Symbol Layer
+/* Keymap 1: Mouse / Numeric Pad Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |        |
@@ -55,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |      | WH_L |      | WH_R |      |      |           |      |      |   1  |   2  |   3  |   \  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | TO(0) |      |      |      |      |                                       |      |    . |   0  |   =  |      |
+ *   | TO(0) |      |      |      |      |                                       |      |    . |   0  |   =  | TO(0) |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -76,14 +77,48 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_TRNS,     KC_TRNS,
                                       KC_BTN1, KC_BTN2, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
 ),
+/* Keymap 2: Experiment Layer
+ *
+ * ,---------------------------------------------------.           ,--------------------------------------------------.
+ * |         |  F13 |  F14 | F15  |  F16 |  F17 |  F18 |           |  F19 |  F20 |  F21 |  F22 |  F23 |  F24 |        |
+ * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
+ * |         | WH_U |      | MS_U |      |      |      |           |      |      |   7  |   8  |   9  |   *  |        |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |         | WH_D | MS_L | MS_D | MS_D |   `  |------|           |------|      |   4  |   5  |   6  |   +  |        |
+ * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |         |      | WH_L |      | WH_R |      |      |           |      |      |   1  |   2  |   3  |   \  |        |
+ * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   | TO(0) |      |      |      |      |                                       |      |    . |   0  |   =  | TO(0) |
+ *   `-----------------------------------'                                       `----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |M_BTN1|M_BTN2|------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
+[EXPR] = LAYOUT_ergodox_pretty(
+  // left hand
+  KC_TRNS, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,    KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_TRNS,
+  KC_TRNS, KC_WH_U, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_TRNS,
+  KC_TRNS, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                     KC_TRNS, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_WH_L, KC_TRNS, KC_WH_R, KC_TRNS, KC_TRNS,   KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+  TO(0),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                       KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  TO(0),
+                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
+                                                        KC_TRNS,     KC_TRNS,
+                                      KC_BTN1, KC_BTN2, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+),
 };
 // clang-format on
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
             case VRSN:
                 SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+                return false;
+            case KC_F13:
+                SEND_STRING("const fName = () => {"SS_TAP(X_ENT));
                 return false;
         }
     }
