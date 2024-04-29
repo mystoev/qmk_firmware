a6ad14ed516dcb7b40be72d42b9f4157376bb7a7
Martin Stoev <martin.stoev@dormakaba.com>
2024-04-29 13:41:41 +0300

    Add lang macro

--- keyboards/ergodox_ez/keymaps/mystoev/keymap.c
+++ keyboards/ergodox_ez/keymaps/mystoev/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   =  |           |  -   |------+-----+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |TO(1) |BL_TOGG| Alt | Ctrl/Left| Gui/Right|                               |Gui/Up|Ctrl/Down|Alt/[|  ] | TO(2)  |
+ *   |TO(1) |KC_LNG1| Alt | Ctrl/Left| Gui/Right|                               |Gui/Up|Ctrl/Down|Alt/[|  ] | TO(2)  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Prev | Next |       | Left | Right |
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,    KC_W,        KC_E,            KC_R,    KC_T,    KC_WH_U,              KC_WH_D,     KC_Y,   KC_U,          KC_I,            KC_O,            KC_P,    KC_BSLS,
   KC_CAPS,    KC_A,    KC_S,        KC_D,            KC_F,    KC_G,                                       KC_H,   KC_J,          KC_K,            KC_L,            KC_SCLN, KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,        KC_C,            KC_V,    KC_B,    KC_EQL,               KC_MINS,     KC_N,   KC_M,          KC_COMM,         KC_DOT,          KC_SLSH, KC_RSFT,
-  TO(1),      BL_TOGG, LALT(KC_NO), LCTL_T(KC_LEFT), LGUI_T(KC_RGHT),                                             RGUI_T(KC_UP), RCTL_T(KC_DOWN), RALT_T(KC_LBRC), KC_RBRC, TO(2),
+  TO(1),      KC_LNG1, LALT(KC_NO), LCTL_T(KC_LEFT), LGUI_T(KC_RGHT),                                             RGUI_T(KC_UP), RCTL_T(KC_DOWN), RALT_T(KC_LBRC), KC_RBRC, TO(2),
                                                                  KC_MPRV, KC_MNXT,         KC_LEFT, KC_RGHT,
                                                                           KC_VOLU,         KC_UP,
                                                          KC_BSPC, KC_ESC, KC_VOLD,         KC_DOWN, KC_ENT, KC_SPC
@@ -116,6 +116,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         switch (keycode) {
             case VRSN:
                 SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+                return false;
+            case KC_LNG1:
+                SEND_STRING(SS_DOWN(X_LALT) SS_DOWN(X_LCTL) SS_TAP(X_SPC) SS_UP(X_LALT) SS_UP(X_LCTL));
                 return false;
             case KC_F13:
                 SEND_STRING("const fName = () => {"SS_TAP(X_ENT));
