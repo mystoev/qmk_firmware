4846e1e416537539757f223293a795fccc1d6c1f
Martin Stoev <martin.y.stoev@gmail.com>
2024-06-30 16:59:23 +0300

    Add one more layer. Change LT swtiches

--- keyboards/ergodox_ez/keymaps/mystoev/keymap.c
+++ keyboards/ergodox_ez/keymaps/mystoev/keymap.c
@@ -5,6 +5,7 @@ enum layers {
     BASE,  // default layer
     MOUSE,  // mouse
     EXPR,  // experiments
+    EXPR1,  // experiments aux
     EXPR2,  // experiments aux
 };
 
@@ -89,14 +90,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|   =  |           |  -   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |TO(0) |KC_LNG1|  -  |      |      |                                       |      |      |   [  |   ]  | TO(0)  |
+ *   |TO(0) |KC_LNG1|     |      |      |                                       |      |      |   [  |   ]  | TO(0)  |
  *   `----------------------------------'                                       `------------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | Prev | Next |       | Left | Right |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 | Back |      | Vol+ |       |  Up  |        |      |
- *                                 | Space| Esc  |------|       |------| Enter  |Space |
- *                                 |      |      | MO(3)|       | MO(3)|        |      |
+ *                                 | LT3  |      | Vol+ |       |  Up  |        | LT3  |
+ *                                 | Back | Esc  |------|       |------| Enter  |Space |
+ *                                 | Space|      | Vol- |       | Down |        |      |
  *                                 `--------------------'       `----------------------'
  */
 [EXPR] = LAYOUT_ergodox_pretty(
@@ -105,14 +106,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,    KC_W,        KC_E,            KC_R,          KC_T,    KC_WH_U,              KC_WH_D,     KC_Y,   KC_U,          KC_I,            KC_O,            KC_P,    KC_BSLS,
   KC_CAPS,    KC_A,    LALT_T(KC_S),LCTL_T(KC_D),    LGUI_T(KC_F),  KC_G,                                       KC_H,   RGUI_T(KC_J),  RCTL_T(KC_K),    RALT_T(KC_L),    KC_SCLN, KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,        KC_C,            KC_V,          KC_B,    KC_EQL,               KC_MINS,     KC_N,   KC_M,          KC_COMM,         KC_DOT,          KC_SLSH, KC_RSFT,
-  TO(0),      KC_LNG1, KC_NO,       KC_TRNS,         KC_TRNS,                                                           KC_TRNS,       KC_TRNS,         KC_LBRC,         KC_RBRC, TO(0),
+  TO(0),      KC_LNG1, KC_TRNS,     KC_TRNS,         KC_TRNS,                                                           KC_TRNS,       KC_TRNS,         KC_LBRC,         KC_RBRC, TO(0),
                                                                  KC_MPRV, KC_MNXT,         KC_LEFT, KC_RGHT,
                                                                           KC_VOLU,         KC_UP,
-                                                         KC_BSPC, KC_ESC, MO(3),           MO(3), KC_ENT, KC_SPC
+                                           LT(3, KC_BSPC), LT(4, KC_ESC), KC_VOLD,         KC_DOWN, LT(4, KC_ENT), LT(3, KC_SPC)
 ),
 
 
 /* Keymap 3: Experiment Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   ~    |   1  |   2  |   3  |   4  |   5  | Play |           | Mute |   6  |   7  |   8  |   9  |   0  |  Del   |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | TAB    |      |      |  Up  |      |      |  MWU |           |  MWD |      |   7  |   8  |   9  |      |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |CapsLock|      | Left | Down | Right|      |------|           |------|      |   4  |   5  |   6  |  ;   |   '    |
+ * |--------+------+------+------+------+------|   =  |           |  -   |------+------+------+------+------+--------|
+ * | LShift |      |      |      |      |      |      |           |      |      |   1  |   2  |   3  |  /   | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |TO(0) |KC_LNG1|  -  |      |      |                                       |   ,  |   0  |   .  |      | TO(0)  |
+ *   `----------------------------------'                                       `-----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        | Prev | Next |       | Left | Right |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 | Back |      | Vol+ |       |  Up  |        |      |
+ *                                 | Space| Esc  |------|       |------| Enter  |Space |
+ *                                 |      |      | Vol- |       | Down |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[EXPR1] = LAYOUT_ergodox_pretty(
+  //left hand
+  KC_GRV,     KC_1,    KC_2,        KC_3,           KC_4,          KC_5,    KC_MPLY,              KC_MUTE,     KC_6,   KC_7,          KC_8,            KC_9,            KC_0,    KC_DEL,
+  KC_TAB,     KC_TRNS, KC_TRNS,     KC_UP,          KC_TRNS,       KC_TRNS, KC_WH_U,              KC_WH_D,     KC_TRNS,KC_7,          KC_8,            KC_9,            KC_TRNS, KC_BSLS,
+  KC_CAPS,    KC_TRNS, KC_LEFT,     KC_DOWN,        KC_RIGHT,      KC_TRNS,                                    KC_TRNS,KC_4,          KC_5,            KC_6,            KC_SCLN, KC_QUOT,
+  KC_LSFT,    KC_TRNS, KC_TRNS,     KC_TRNS,        KC_TRNS,       KC_TRNS, KC_EQL,               KC_MINS,     KC_TRNS,KC_1,          KC_2,            KC_3,            KC_SLSH, KC_RSFT,
+  TO(0),      KC_LNG1, KC_NO,       KC_TRNS,        KC_TRNS,                                                           KC_COMM,       KC_0,            KC_DOT,          KC_TRNS, TO(0),
+                                                                 KC_MPRV, KC_MNXT,         KC_LEFT, KC_RGHT,
+                                                                          KC_VOLU,         KC_UP,
+                                                         KC_BSPC, KC_ESC, KC_VOLD,         KC_DOWN, KC_ENT, KC_SPC
+),
+
+/* Keymap 4: Experiment Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   ~    |   1  |   2  |   3  |   4  |   5  | Play |           | Mute |   6  |   7  |   8  |   9  |   0  |  Del   |
