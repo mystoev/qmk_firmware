c399640fe83a68df0af7b5ae1dd7cac2cf64cc2d
Martin Stoev <martin.y.stoev@gmail.com>
2024-10-04 16:59:40 +0300

    Update keymap.c

--- keyboards/ergodox_ez/keymaps/mystoev/keymap.c
+++ keyboards/ergodox_ez/keymaps/mystoev/keymap.c
@@ -7,6 +7,9 @@ enum layers {
     EXPR,  // experiments
     EXPR1,  // experiments aux
     EXPR2,  // experiments aux
+    CORNE1,
+    CORNE2,
+    CORNE3,
 };
 
 enum custom_keycodes {
@@ -18,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ~    |   1  |   2  |   3  |   4  |   5  | Play |           | Mute |   6  |   7  |   8  |   9  |   0  |  Del   |
+ * |   ~    |   1  |   2  |   3  |   4  |   5  | TO(5)|           | Mute |   6  |   7  |   8  |   9  |   0  |  Del   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | TAB    |   Q  |   W  |   E  |   R  |   T  |  MWU |           |  MWD |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -38,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_ergodox_pretty(
   //left hand
-  KC_GRV,     KC_1,    KC_2,        KC_3,            KC_4,    KC_5,    KC_MPLY,              KC_MUTE,     KC_6,   KC_7,          KC_8,            KC_9,            KC_0,    KC_DEL,
+  KC_GRV,     KC_1,    KC_2,        KC_3,            KC_4,    KC_5,    TO(5),              KC_MUTE,     KC_6,   KC_7,          KC_8,            KC_9,            KC_0,    KC_DEL,
   KC_TAB,     KC_Q,    KC_W,        KC_E,            KC_R,    KC_T,    KC_WH_U,              KC_WH_D,     KC_Y,   KC_U,          KC_I,            KC_O,            KC_P,    KC_BSLS,
   KC_CAPS,    KC_A,    KC_S,        KC_D,            KC_F,    KC_G,                                       KC_H,   KC_J,          KC_K,            KC_L,            KC_SCLN, KC_QUOT,
   KC_LSFT,    KC_Z,    KC_X,        KC_C,            KC_V,    KC_B,    KC_EQL,               KC_MINS,     KC_N,   KC_M,          KC_COMM,         KC_DOT,          KC_SLSH, KC_RSFT,
@@ -178,13 +181,126 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                           KC_VOLU,         KC_UP,
                                                          KC_BSPC, KC_ESC, KC_VOLD,         KC_DOWN, KC_ENT, KC_SPC
 ),
+
+/* Keymap 5: Corne Layout Layer 1
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   -    |   -  |   -  |   -  |   -  |   -  |TO(0) |           |   -  |   -  |   -  |   -  |   -  |   -  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | TAB    |   Q  |   W  |  E   |   R  |   T  |   -  |           |   -  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |    -   |   A  |   S  |  D   |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
+ * |--------+------+------+------+------+------|   -  |           |   -  |------+------+------+------+------+--------|
+ * |   -    |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |  -   |   -  |  -  |   -  |   -  |                                       |   -  |   -  |   -  |   -  |    -  |
+ *   `----------------------------------'                                       `-----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |  -   |  -   |       |   -  |   -   |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 | LT7  | LT6  |  -   |       |   -  | LT7    | LT6  |
+ *                                 | Back | Esc  |------|       |------| Enter  |Space |
+ *                                 | Space|      | Vol- |       | Down |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[CORNE1] = LAYOUT_ergodox_pretty(
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO,   KC_NO, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+  KC_NO, LALT_T(KC_A), LSFT_T(KC_S), LCTL_T(KC_D), LGUI_T(KC_F), KC_G,   KC_H, RGUI_T(KC_J), RCTL_T(KC_K), RSFT_T(KC_L), RALT_T(KC_SCLN), KC_QUOT,
+  KC_NO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO,   KC_NO, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
+
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_NO, KC_NO, KC_NO, KC_NO,
+  KC_NO, KC_NO,
+  LT(7, KC_BSPC), LT(6, KC_ESC), KC_VOLD,   KC_DOWN, LT(7, KC_ENT), LT(6, KC_SPC)
+),
+
+/* Keymap 6: Corne Layout Layer 2
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   -    |   -  |   -  |   -  |   -  |   -  |TO(0) |           |   -  |   -  |   -  |   -  |   -  |   -  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | TAB    |   1  |   2  |  3   |   4  |   5  |   -  |           |   -  |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |   -    |   -  |   -  |  -   |   -  |   -  |------|           |------| Left | Down |  Up  |Right |  -   |   -    |
+ * |--------+------+------+------+------+------|   -  |           |   -  |------+------+------+------+------+--------|
+ * |  -     |   -  |   -  |   -  |   -  |   -  |      |           |      |   -  |   -  |   -  |   -  |  -   |   -    |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |  -   |   -  |  -  |   -  |   -  |                                       |   -  |   -  |   -  |   -  |    -  |
+ *   `----------------------------------'                                       `-----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |  -   |  -   |       |   -  |   -   |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 | Back |      |  -   |       |   -  |        |      |
+ *                                 | Space| Esc  |------|       |------| Enter  |Space |
+ *                                 |      |      | Vol- |       | Down |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[CORNE2] = LAYOUT_ergodox_pretty(
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_TAB, KC_1, KC_2, KC_3, KC_4, KC_5, KC_NO,   KC_NO, KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO,
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO, KC_NO,
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_NO, KC_NO, KC_NO, KC_NO,
+  KC_NO, KC_NO,
+  KC_BSPC, KC_ESC, KC_VOLD,   KC_DOWN, KC_ENT, KC_SPC
+),
+
+/* Keymap 7: Corne Layout Layer 3
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |   -    |   -  |   -  |   -  |   -  |   -  |TO(0) |           |   -  |   -  |   -  |   -  |   -  |   -  |   -    |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * | TAB    |   !  |   @  |  #   |   $  |   %  |   -  |           |   -  |   ^  |   &  |   *  |   (  |   )  |   -    |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |    -   |   -  |   -  |  -   |   -  |   -  |------|           |------|  -_  |  +=  |  [{  |  }]  |  \|  |   -    |
+ * |--------+------+------+------+------+------|   -  |           |   -  |------+------+------+------+------+--------|
+ * |  -     |   -  |   -  |   -  |   -  |   -  |      |           |      |   -  |   +  |   {  |   }  |  |   |   -    |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |  -   |   -  |  -  |   -  |   -  |                                       |   -  |   -  |   -  |   -  |    -  |
+ *   `----------------------------------'                                       `-----------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |  -   |  -   |       |   -  |   -   |
+ *                                 ,------|------|------|       |------+--------+------.
+ *                                 | Back |      |  -   |       |   -  |        |      |
+ *                                 | Space| Esc  |------|       |------| Enter  |Space |
+ *                                 |      |      | Vol- |       | Down |        |      |
+ *                                 `--------------------'       `----------------------'
+ */
+[CORNE3] = LAYOUT_ergodox_pretty(
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, TO(0), KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_TAB, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_NO,   KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS, KC_NO,
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, KC_NO,
+
+  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+
+  KC_NO, KC_NO, KC_NO, KC_NO,
+  KC_NO, KC_NO,
+  KC_BSPC, KC_ESC, KC_VOLD,   KC_DOWN, KC_ENT, KC_SPC
+),
+
 };
+
+const uint16_t PROGMEM caps_lock_combo[] = {KC_L, KC_S, COMBO_END};
+const uint16_t PROGMEM lang_combo[] = {KC_SPC, KC_BSPC, COMBO_END};
+combo_t key_combos[] = {
+    COMBO(caps_lock_combo, KC_CAPS),
+    COMBO(lang_combo, KC_LNG1),
+};
+
 // clang-format on
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
-            case VRSN:
-                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+            case VRSN: SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
                 return false;
             case KC_LNG1:
                 SEND_STRING(SS_DOWN(X_LALT) SS_DOWN(X_LCTL) SS_TAP(X_SPC) SS_UP(X_LALT) SS_UP(X_LCTL));
@@ -267,4 +383,4 @@ layer_state_t layer_state_set_user(layer_state_t state) {
     }
 
     return state;
-};
+}
